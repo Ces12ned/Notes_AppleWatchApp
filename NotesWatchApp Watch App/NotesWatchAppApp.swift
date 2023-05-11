69166c98f7a72c8a1e3908e8da9539d06c678fee
Edgar Cisneros <cisns.edg@gmail.com>
2023-05-10 22:43:37 -0600

    Base project.

---
+++ NotesWatchApp Watch App/NotesWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesWatchAppApp.swift
+//  NotesWatchApp Watch App
+//
+//  Created by Edgar Cisneros on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
